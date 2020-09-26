5b46da4d155a8c10c617a54f2f655554ffc9ec2f
kereemsunay <sunay.kerem@outlook.com>
2020-09-26 20:09:40 +0300

    Initial Commit

---
+++ MoviesSwiftUI/MoviesSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesSwiftUIApp.swift
+//  MoviesSwiftUI
+//
+//  Created by Kerem on 26.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct MoviesSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
